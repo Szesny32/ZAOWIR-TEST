5aa8c33cfcf83a116fff948397cf83e4162b9854
Szesny32 <bartoszszczesny.studia@gmail.com>
2023-12-14 02:30:44 +0100

    poprawka

--- test.py
+++ test.py
@@ -12,17 +12,24 @@ CHECKBOARD_PATTERN = (7, 10)
 CHECKBOARD_NO_CORNERS = 70
 SQUARE_SIZE = 44
 MARKER_SIZE = 34
-
+UNIT = 'mm'
 
 CHARUCO_DICT = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_6X6_50)
 CHARUCO_BOARD = cv.aruco.CharucoBoard(CHECKBOARD_SIZE, SQUARE_SIZE, MARKER_SIZE, CHARUCO_DICT)
 DETECTOR_PARAMETERS = cv.aruco.DetectorParameters()
 CHARUCO_PARAMS = cv.aruco.CharucoParameters()
 CHARUCO_DETECTOR = cv.aruco.CharucoDetector(CHARUCO_BOARD, CHARUCO_PARAMS, DETECTOR_PARAMETERS)
+
 DEBUG = False
 
-#------------------------------------------------------------------------------------------
 
+LEFT_PATH = 'datasets/Charuco3/'
+RIGHT_PATH = 'datasets/Charuco3/'
+
+RED = (0, 0, 255)
+GREEN = (0, 255, 0)
+objPointsArray = []
+#------------------------------------------------------------------------------------------
 
 
 def LoadDataset(path, prefix, extension):
@@ -31,29 +38,25 @@ def LoadDataset(path, prefix, extension):
     assert len(dataset) > 0
     return dataset
 
+def initImageSize(sample):
+    image = cv.imread(sample)
+    height, width = image.shape[:2]
+    return (width, height)
 
 def Calibration(_DATASET):
+    imageSize = initImageSize(_DATASET[0])
     #Szukanie narożników
     imgPoints = []
-    #_objPoints = []
     for sample in tqdm(_DATASET):
         image = cv.imread(sample)
-        height, width = image.shape[:2]
 
         corners, ids, rejectedImgPoints = cv.aruco.detectMarkers(image, CHARUCO_DICT)
         response, charuco_corners, charuco_ids = cv.aruco.interpolateCornersCharuco(corners, ids, image, CHARUCO_BOARD)
-
         #charuco_corners, charuco_ids, marker_corners, marker_ids = CHARUCO_DETECTOR.detectBoard(image)
-        
-        
 
         if len(charuco_corners) == CHECKBOARD_NO_CORNERS:
             #_, imgPts = cv.aruco.getBoardObjectAndImagePoints(CHARUCO_BOARD, charuco_corners, charuco_ids)
             imgPoints.append(charuco_corners)
-            #imgPoints.append(imgPts)
-            #imgPoints_ids.append(charuco_ids)
-            #_objPoints.append(objPoints)
-
 
         if DEBUG == True:
             image = cv.aruco.drawDetectedCornersCharuco(image, charuco_corners, charucoIds=None, cornerColor=(0,0,255))
@@ -76,7 +79,7 @@ def Calibration(_DATASET):
 
 
 
-    retval, cameraMatrix, distCoeffs, rvecs, tvecs =  cv.calibrateCamera(objPointsArray, imgPoints, imageSize =  (width, height), cameraMatrix= None, distCoeffs= None) 
+    retval, cameraMatrix, distCoeffs, rvecs, tvecs =  cv.calibrateCamera(objPointsArray, imgPoints, imageSize, cameraMatrix= None, distCoeffs= None) 
     print(f"Zakończono proces kalibracji w {(time() - timestamp):.2f}s\n")
     return (retval, cameraMatrix, distCoeffs, rvecs, tvecs, imgPoints) 
     
@@ -93,10 +96,9 @@ def GenerateObjectPoints():
     objp[:, :2] = np.mgrid[0 : CHECKBOARD_PATTERN[0], 0 : CHECKBOARD_PATTERN[1]].T.reshape(-1, 2) * SQUARE_SIZE
     return objp
 
-def CalculateFOV(cameraMatrix, sample):
+def CalculateFOV(cameraMatrix, imageSize):
 
-    image = cv.imread(sample)
-    height, width = image.shape[:2]
+    (width, height)  = imageSize
 
     fx = cameraMatrix[0][0]
     fy = cameraMatrix[1][1]
@@ -106,9 +108,7 @@ def CalculateFOV(cameraMatrix, sample):
 
     return fovW, fovH
 
-def StereoCalibration(calibration1, calibration2, sample):
-    image = cv.imread(sample)
-    height, width = image.shape[:2]
+def StereoCalibration(calibration1, calibration2, imageSize):
     (_, cameraMatrix1, distCoeffs1, _, _, imgPoints1) = calibration1
     (_, cameraMatrix2, distCoeffs2, _, _, imgPoints2) = calibration2
 
@@ -118,11 +118,11 @@ def StereoCalibration(calibration1, calibration2, sample):
     stereoData = cv.stereoCalibrate(objPointsArray, imgPoints1, imgPoints2, 
                         cameraMatrix1, distCoeffs1,
                         cameraMatrix2, distCoeffs2, 
-                        imageSize = (width, height))
+                        imageSize)
     return stereoData
 
 def ExportCalibration(filepath, data):
-    (retval,  cameraMatrix,  distCoeffs,  rvecs,  tvecs) = data
+    (retval,  cameraMatrix,  distCoeffs,  rvecs,  tvecs, _) = data
     data = {
         "retval" : retval,
         "cameraMatrix" : cameraMatrix.tolist(), 
@@ -151,65 +151,53 @@ def ExportStereo(path, data):
         json.dump(jsonData, file, separators=(',', ': '), indent = 4)
         print(f"Konfiguracja kalibracyjna została pomyślnie zapisana w pliku {path}\n")
 
+def Rectify(image, cameraMatrix, distCoeffs, R1, P1, sroi, IMAGE_SIZE):
+    map1, map2 = cv.initUndistortRectifyMap(cameraMatrix, distCoeffs, R1, P1, IMAGE_SIZE, m1type=cv.CV_32FC1)
+    rect = cv.remap(image, map1, map2, interpolation=cv.INTER_LINEAR)
+    cv.rectangle(rect, (sroi[0], sroi[1]), (sroi[2], sroi[3]), color=GREEN, thickness=2)
+    return rect
 
 #----------------------------------------------
-LEFT_PATH = 'datasets/Charuco3/'
+#[...] Wczytatanie danych
 LEFT_DATASET = LoadDataset(LEFT_PATH, prefix='left', extension='.png')
-
-RIGHT_PATH = 'datasets/Charuco3/'
 RIGHT_DATASET = LoadDataset(RIGHT_PATH, prefix='right', extension='.png')
-#-
-LEFT_CALIBRATION = Calibration(LEFT_DATASET)
-(retval, cameraMatrix, distCoeffs, rvecs, tvecs, imgPoints)  = LEFT_CALIBRATION
-ExportCalibration('LEFT_CALIBRATION.json', data = (retval, cameraMatrix, distCoeffs, rvecs, tvecs))
+IMAGE_SIZE = initImageSize(LEFT_DATASET[0])
 
+
+#[#..] Kalibracja
+LEFT_CALIBRATION = Calibration(LEFT_DATASET)
+ExportCalibration('LEFT_CALIBRATION.json', data = LEFT_CALIBRATION)
 
 RIGHT_CALIBRATION = Calibration(RIGHT_DATASET)
-(retval, cameraMatrix, distCoeffs, rvecs, tvecs, imgPoints)  = RIGHT_CALIBRATION
-ExportCalibration('RIGHT_CALIBRATION.json', data = (retval, cameraMatrix, distCoeffs, rvecs, tvecs))
+ExportCalibration('RIGHT_CALIBRATION.json', data = RIGHT_CALIBRATION)
 
 
-
-stereoData = StereoCalibration(LEFT_CALIBRATION, RIGHT_CALIBRATION, LEFT_DATASET[0])
-(retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, rotationMat, translationVec, essentialMat, fundamentalMat) =  stereoData
+#[##.] Stereo
+stereoData = StereoCalibration(LEFT_CALIBRATION, RIGHT_CALIBRATION, IMAGE_SIZE)
+(retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, rotationMat, translationVec, essentialMat, fundamentalMat) = stereoData
 ExportStereo('STEREO_CALIBRATION.json', stereoData)
 
+baseline_mm = np.linalg.norm(translationVec)
+baseline_cm = baseline_mm * 0.1
+print(f"Odległość bazowa (baseline): {baseline_cm:.2f} cm")
+print(f'LEFT FOV: {CalculateFOV(cameraMatrix1, IMAGE_SIZE)}')
+print(f'RIGHT FOV: {CalculateFOV(cameraMatrix2, IMAGE_SIZE)}')
 
-baseline = round(np.linalg.norm(translationVec), 2)
-print(f"Odległość bazowa (baseline): {baseline/10:.2f} CM")
 
-print(f'LEFT FOV: {CalculateFOV(cameraMatrix1, LEFT_DATASET[0])}')
-print(f'RIGHT FOV: {CalculateFOV(cameraMatrix2, RIGHT_DATASET[0])}')
-
-
+#[###] Rektyfikacja
 img1 = cv.imread(LEFT_DATASET[0])
 img2 = cv.imread(RIGHT_DATASET[0])
 
-(height, width) = img1.shape[:2]
+R1, R2, P1, P2, Q, sroi1, sroi2 = cv.stereoRectify(cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, IMAGE_SIZE, R=rotationMat, T=translationVec, alpha=1)
 
+rect1 = Rectify(img1, cameraMatrix1, distCoeffs1, R1, P1, sroi1, IMAGE_SIZE)
+rect2 = Rectify(img2, cameraMatrix2, distCoeffs2, R2, P2, sroi2, IMAGE_SIZE)
+outputImg = np.concatenate((rect1, rect2), axis=1)
+height, width = outputImg.shape[:2]
 
-
-R1, R2, P1, P2, Q, sroi1, sroi2 = cv.stereoRectify(cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, (width, height), rotationMat, translationVec, alpha=0.9)
-map1_cam1, map2_cam1 = cv.initUndistortRectifyMap(cameraMatrix1, distCoeffs1, R1, P1, (width, height), m1type=cv.CV_32FC1)
-map1_cam2, map2_cam2 = cv.initUndistortRectifyMap(cameraMatrix2, distCoeffs2, R2, P2, (width, height), m1type=cv.CV_32FC1)
-
-
-
-
-
-remap_method = cv.INTER_LINEAR
-imgAfterRect1 = cv.remap(img1, map1_cam1, map2_cam1, remap_method)
-imgAfterRect2 = cv.remap(img2, map1_cam2, map2_cam2, remap_method)    
-cv.rectangle(imgAfterRect1, (sroi1[0], sroi1[1]), (sroi1[2], sroi1[3]), (0, 0, 255), 2)
-cv.rectangle(imgAfterRect2, (sroi2[0], sroi2[1]), (sroi2[2], sroi2[3]), (0, 0, 255), 2)
-
-
-
-catImgsAfterRect = np.concatenate((imgAfterRect1, imgAfterRect2), axis=1)
-for i in range(10, np.shape(catImgsAfterRect)[0], 50):
-    cv.line(catImgsAfterRect, (0, i), (np.shape(catImgsAfterRect)[1], i), (0, 0, 255), 1)
-
-cv.imwrite("catImgsAfterRect.png", catImgsAfterRect)
+for i in range(10, height, 50):
+    cv.line(outputImg, (0, i), (width, i), color=RED,  thickness=2)
+cv.imwrite("outputImg.png", outputImg)
 
 
 
